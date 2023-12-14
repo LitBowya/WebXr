b9f3344ba63a8d8ecc1164f9035fc7eb4e5d808e
LitBowya <agbagabenjamin819@gmail.com>
2023-12-14 18:50:37 +0000

    commits

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+// src/ARScene.js
 
-const App = () => {
+import React, { useEffect, useState } from "react";
+import "aframe";
+
+const ARScene = () => {
+  const [isARSupported, setIsARSupported] = useState(false);
+
+  useEffect(() => {
+    // Check if AR is supported in the browser
+    if (window && window.navigator.xr) {
+      window.navigator.xr.isSessionSupported('immersive-ar').then((supported) => {
+        setIsARSupported(supported);
+      });
+    }
+  }, []);
+
   return (
-    <div>App</div>
-  )
-}
+    <div style={{ height: "100vh" }}>
+      {isARSupported ? (
+        <a-scene embedded arjs="sourceType: webcam;">
+          <a-box position="0 0.5 0" rotation="0 45 0" color="#4CC3D9" shadow />
+          <a-marker preset="hiro">
+            <a-box position="0 0.5 0" rotation="0 45 0" color="#FFC65D" shadow />
+          </a-marker>
+          <a-entity camera></a-entity>
+        </a-scene>
+      ) : (
+        <p>Your browser doesn't support AR. Please use a compatible browser.</p>
+      )}
+    </div>
+  );
+};
 
-export default App
+export default ARScene;
