7eb3df16137bafe7eb66c32b3eba4a5cad49a7d8
LitBowya <agbagabenjamin819@gmail.com>
2023-12-14 18:38:50 +0000

    commits

--- webxr/src/App.jsx
+++ webxr/src/App.jsx
@@ -1,18 +1,40 @@
 // src/ARScene.js
 
-import React from "react";
+import { useEffect, useState } from "react";
 import "aframe";
 
 const ARScene = () => {
+  const [isARSupported, setIsARSupported] = useState(false);
+
+  useEffect(() => {
+    // Check if AR is supported in the browser
+    if (window && window.navigator.xr) {
+      window.navigator.xr
+        .isSessionSupported("immersive-ar")
+        .then((supported) => {
+          setIsARSupported(supported);
+        });
+    }
+  }, []);
+
   return (
     <div style={{ height: "100vh" }}>
-      <a-scene embedded arjs="sourceType: webcam;">
-        <a-box position="0 0.5 0" rotation="0 45 0" color="#4CC3D9" shadow />
-        <a-marker preset="hiro">
-          <a-box position="0 0.5 0" rotation="0 45 0" color="#FFC65D" shadow />
-        </a-marker>
-        <a-entity camera></a-entity>
-      </a-scene>
+      {isARSupported ? (
+        <a-scene embedded arjs="sourceType: webcam;">
+          <a-box position="0 0.5 0" rotation="0 45 0" color="#4CC3D9" shadow />
+          <a-marker preset="hiro">
+            <a-box
+              position="0 0.5 0"
+              rotation="0 45 0"
+              color="#FFC65D"
+              shadow
+            />
+          </a-marker>
+          <a-entity camera></a-entity>
+        </a-scene>
+      ) : (
+        <p>Your browser does not support AR. Please use a compatible browser.</p>
+      )}
     </div>
   );
 };
